e9e8cb0fff453c430dfcda8f02f431d01db73aa9
David Shaked <ds2643@columbia.edu>
2016-11-06 21:47:15 -0500

    some updates

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 An imperative implementation of a mutation testing framework in Python. Obviously, tests serve the purpose in the software development process of ensuring that code behaves as intended. However, given some software project with a test suite too large to inspect by manually, how can we be sure the tests indeed accomplish this goal? That is, we need a tool to make sure the tests cover as much behavior of the program as possible.
 
 Mutation testing helps us measure the sensitivity of a project's tests to bugs.
+anyNote: See mutcl for a sister project written in Clojure (a functional lisp hosted on the JVM) that serves the same purpose as this project.
 
-Note: See mutcl for a sister project written in Clojure (a functional lisp hosted on the JVM) that serves the same purpose as this project.
+Note, the weakness of doing this in a dynamically typed language is the introduction of bias: Transformation of programmatic nodes in an unsupervised manner is not possible because nodes cannot be type-checked. Instead, mutations are only possible for a subset of the target language. In truth, this tool doesn't simulate a randomly selected subset of all possible mutations, but rather a randomly selected subset of some mutations.
 
 # Notes:
 1. AST Module: Initially, interface of standard AST module preferred over RedBaron, but RedBaron doesn't support dumping the ast, which is needed to run the test suite natively.
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 mutation testing in python
 '''
 from redbaron import RedBaron
+import redbaron.nodes as n
 
 def program_as_ast(path):
     ''' returns program (specified by path) as Python abstract syntax tree'''
@@ -17,22 +18,40 @@ def recover_program(ast):
 def validate_ast(ast):
     ''' returns boolean indicating if program runs'''
     try:
-        exec(ast.dumps())
+        exec(ast.dumps()) # TODO: redirect outpurt
         return True
     except:
         return False
 
-def select_random_ast_node(ast):
-    ''' returns a randomly selected node of ast '''
+def safety_validation(ast):
+    ''' ensures program has no harmful side effects '''
     return None
 
-def substitute_ast_node(node):
-    ''' swaps an ast node for another ast node '''
-    return None
+#TODO for now, hard code mutations into walk
 
-def collect_test_results():
-    ''' collect unittest python results '''
-    return None
+def walk_ast(node):
+    ''' given the (root) node of an abstract syntax tree, walk the tree conditionally based on node class... ripe for substitution appliction: credit to andrea orro and james katz'''
+    try:
+        if isinstance(node, n.AssignmentNode):
+            walk_ast(node.value)
+        elif isinstance(node, n.DefNode):
+            walk_ast(node.value)
+        elif isinstance(node, n.NameNode):
+            pass
+        elif isinstance(node, n.ReturnNode):
+            walk_ast(node.value)
+        elif isinstance(node, n.AssociativeParenthesisNode):
+            walk_ast(node.value)
+        elif isinstance(node, n.BinaryOperatorNode):
+            walk_ast(node.first)
+            walk_ast(node.second)
+        else:
+            for child in node:
+                walk_ast(child)
+    except TypeError:
+        return None
 
 if __name__ == "__main__":
-    print("0")
+    ast = program_as_ast(argv[1])
+    if validate_ast(ast):
+
