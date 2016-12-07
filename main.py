79e16886f0c1179873819fc5d247d0e8098c2edb
David Shaked <ds2643@columbia.edu>
2016-12-06 20:26:18 -0500

    restrict possible transformations to constants

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ from redbaron import RedBaron
 import redbaron.nodes as n
 import ast # used for count_ast_nodes
 
+# TODO: substitution function node types: https://github.com/PyCQA/redbaron/blob/497a55f51a1902f67b30519c126469e60b4f569f/docs/nodes_reference.rst
+
+TRANSFORMATIONS = {
+    #subset of nodes that occur in RedBarron involving constant expressions
+            "AssertNode": lambda x: x,
+            "AssignmentNode": lambda x: x,
+            "AssociativeParenthesisNode": lambda x: x,
+            "BinaryNode": lambda x: x,
+            "BinaryOperatorNode": lambda x: x,
+            "BooleanOperatorNode": lambda x: x,
+            "ComparisonNode": lambda x: x,
+            "DictNode": lambda x: x,
+            "IntNode": lambda x: x,
+            "ListNode": lambda x: x,
+            "SetNode": lambda x: x,
+            "SliceNode": lambda x: x,
+            "StringChainNode": lambda x: x}
+
 def program_as_ast(path):
     ''' returns program (specified by path) as Python abstract syntax tree (Redbaron)'''
     # retrofit with try/except
@@ -46,8 +64,6 @@ def count_ast_nodes(ast_alt):
     ''' counts nodes in ast given ast module representation of program '''
     return NodeCounter().generic_visit(alt_ast)
 
-# TODO: substitution function node types: https://github.com/PyCQA/redbaron/blob/497a55f51a1902f67b30519c126469e60b4f569f/docs/nodes_reference.rst
-
 def random_mutation(node, tranformations, p_mutation):
     ''' apply type-appropriate mutation to node with specified probability'''
     assert 0 < p_mutation < 1, ""
@@ -62,74 +78,12 @@ def verify_mutation(ast_a, ast_b):
     ''' returns boolean indicating if mutation succeeded in morphing genotype '''
     return ast_a.dump() != ast_b.dump()
 
-def mutate_ast(ast, no_nodes):
+def mutate_ast(ast, no_nodes, trans = TRANSFORMATIONS):
     ''' returns mutant version of programmatic ast given redbaron representation and number of nodes '''
     ast_copy = ast.copy() # single node copy or recursive copy?
     total_nodes = no_nodes
-    p_tranformation = 1/total_nodes # set probability of transforming a node as a function of total number of nodes
-    transformations = dict() # tranformations as a function of node type
-    transformations.update({
-            AssertNode: lambda x: x,
-            AssignmentNode: lambda x: x,
-            AssociativeParenthesisNode: lambda x: x,
-            AtomtrailersNode: lambda x: x,
-            BinaryNode: lambda x: x,
-            BinaryOperatorNode: lambda x: x,
-            BooleanOperatorNode: lambda x: x,
-            CallNode: lambda x: x,
-            CallArgumentNode: lambda x: x,
-            ClassNode: lambda x: x,
-            CommaNode: lambda x: x,
-            ComparisonNode: lambda x: x,
-            ComprehensionIfNode: lambda x: x,
-            ComprehensionLoopNode: lambda x: x,
-            DecoratorNode: lambda x: x,
-            DefArgumentNode: lambda x: x,
-            DelNode: lambda x: x,
-            DictArgumentNode: lambda x: x,
-            DictNode: lambda x: x,
-            DictComprehensionNode: lambda x: x,
-            DottedAsNameNode: lambda x: x,
-            DotNode: lambda x: x,
-            ElifNode: lambda x: x,
-            ElseNode: lambda x: x,
-            EndlNode: lambda x: x,
-            ExceptNode: lambda x: x,
-            ExecNode: lambda x: x,
-            FinallyNode: lambda x: x,
-            ForNode: lambda x: x,
-            FromImportNode: lambda x: x,
-            FuncdefNode: lambda x: x,
-            GeneratorComprehensionNode: lambda x: x,
-            GetitemNode: lambda x: x,
-            GlobalNode: lambda x: x,
-            IfNode: lambda x: x,
-            IfelseblockNode: lambda x: x,
-            ImportNode: lambda x: x,
-            IntNode: lambda x: x,
-            LambdaNode: lambda x: x,
-            ListArgumentNode: lambda x: x,
-            ListComprehensionNode: lambda x: x,
-            ListNode: lambda x: x,
-            NameAsNameNode: lambda x: x,
-            PrintNode: lambda x: x,
-            RaiseNode: lambda x: x,
-            ReprNode: lambda x: x,
-            ReturnNode: lambda x: x,
-            SetNode: lambda x: x,
-            SetComprehensionNode: lambda x: x,
-            SliceNode: lambda x: x,
-            SpaceNode: lambda x: x,
-            StringChainNode: lambda x: x,
-            TernaryOperatorNode: lambda x: x,
-            TryNode: lambda x: x,
-            UnitaryOperatorNode: lambda x: x,
-            YieldNode: lambda x: x,
-            YieldAtomNode: lambda x: x,
-            WhileNode: lambda x: x,
-            WithContextItemNode: lambda x: x,
-            WithNode: lambda x: x})
-    random_mutation(ast_copy, tranformations, p_mutation)
+    p_tranformation = 1/total_nodes
+    return random_mutation(ast_copy, tran, p_mutation)
 
 if __name__ == "__main__":
     ast = program_as_ast(sys.argv[1])
