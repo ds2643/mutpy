63b4551e7a6dcfe91c594cc22c5d5190e7c1e40c
David Shaked <ds2643@columbia.edu>
2016-12-08 15:56:32 -0500

    tests work in progress

--- test.py
+++ test.py
@@ -8,49 +8,62 @@ from redbaron import RedBaron
 def test_BinaryNode():
     str_repr_i= "0b10101"
     mock_program = RedBaron(string_repr_i)
-    str_repr_f = m.recover_program(_)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_BinaryOperatorNode():
     str_repr_i = "1 + 1"
     mock_program = RedBaron(str_repr_i)
-    str_repr_f = m.recover_program(_)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_BooleanOperatorNode():
-    mock_program = RedBaron("x and y")
-    str_repr_f = m.recover_program(_)
+    str_repr_i= "x and y"
+    mock_program = RedBaron(str_repr_i)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_ComparisonNode():
-    mock_program = RedBaron("42 > 30")
-    str_repr_f = m.recover_program(_)
+    str_repr_i= "42 > 30"
+    mock_program = RedBaron(str_repr_i)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_DictNode():
-    mock_program = RedBaron("{'a': 1, 'b':2, 'c': 3}")
-    str_repr_f = m.recover_program(_)
+    str_repr_i= "{'a': 1, 'b':2, 'c': 3}"
+    mock_program = RedBaron(str_repr_i)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_IntNode():
-    mock_program = RedBaron("42")
-    str_repr_f = m.recover_program(_)
+    str_repr_i= "42"
+    mock_program = RedBaron(str_repr_i)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_ListNode():
-    mock_program = RedBaron("[1, 2, 3]")
-    str_repr_f = m.recover_program(_)
+    str_repr_i= "[1, 2, 3]"
+    mock_program = RedBaron(str_repr_i)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_SetNode():
-    mock_program = RedBaron("{1, 2, 3}")
-    str_repr_f = m.recover_program(_)
+    str_repr_i= "{1, 2, 3}"
+    mock_program = RedBaron(str_repr_i)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
 
 def test_StringNode():
-    mock_program = RedBaron("\"hello world\"")
-    str_repr_f = m.recover_program(_)
+    str_repr_i= "\"hello world\""
+    mock_program = RedBaron(str_repr_i)
+    mutant = m.mutate(mock_program)
+    str_repr_f = m.recover_program(mutant)
     assert(str_repr_f != str_repr_i)
-
-
-
